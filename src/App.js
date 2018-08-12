13193a0b90947962e37f3f194571018d96dd9629
thienban <thienban91@hotmail.com>
2018-08-12 22:40:13 +0200

    finish ex

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+  state = {
+    userName:'Tban',
+  }
+  handlechange = (event) => {
+    this.setState({userName: event.target.value});
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">React exo1</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <UserInput changed={this.handlechange}
+          currentName={this.state.userName}/>
+          <UserOutput name={this.state.userName}/>
+          <UserOutput name={this.state.userName}/>
+          <UserOutput name={this.state.userName}/>
         </p>
       </div>
     );
